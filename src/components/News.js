9b606e995f92b2787ff968dc61827f24df8960b8
Deepansh Dubey <56029566+deepanshdubey@users.noreply.github.com>
2023-05-07 18:57:14 +0530

    fixing extra space over loading

--- src/components/News.js
+++ src/components/News.js
@@ -1,43 +1,41 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
-import PropTypes from 'prop-types'
-
-
-
+import PropTypes from "prop-types";
 
 export default class News extends Component {
-
   capitalizeFirstLetter = (string) => {
-    return (string.charAt(0).toUpperCase() + string.slice(1));
-  }
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
   static defaultProps = {
-    country: 'in',
+    country: "in",
     pageSize: 12,
-    category: 'general'
-  }
+    category: "general",
+  };
   static propTypes = {
     country: PropTypes.string,
     pageSize: PropTypes.number,
-    category: PropTypes.string
-  }
-  articles = []
+    category: PropTypes.string,
+  };
+  articles = [];
 
   constructor(props) {
     super(props);
     this.state = {
       articles: this.articles,
       loading: false,
-      page: 1
+      page: 1,
     };
 
-    document.title = `${this.capitalizeFirstLetter(this.props.category)} | KhabarWala - Daily News` 
+    document.title = `${this.capitalizeFirstLetter(
+      this.props.category
+    )} | KhabarWala - Daily News`;
   }
 
-  async updateNews(){
+  async updateNews() {
     // const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f5f227c16c604c9ea4eb9d56af9abe6d&q&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    const url = `https://gnews.io/api/v4/top-headlines?category=${this.props.category}&country=${this.props.country}&apikey=b69d624070436ebf4f65259760c33985&max=${this.props.pageSize}`
+    const url = `https://gnews.io/api/v4/top-headlines?category=${this.props.category}&country=${this.props.country}&apikey=b69d624070436ebf4f65259760c33985&max=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -46,29 +44,31 @@ export default class News extends Component {
       totalResults: parsedData.totalResults,
       loading: false,
     });
-
   }
 
   async componentDidMount() {
     this.updateNews();
   }
 
-
   handlePrevClick = async () => {
-    this.setState({page: this.state.page - 1});
+    this.setState({ page: this.state.page - 1 });
     this.updateNews();
   };
 
   handleNextClick = async () => {
-    this.setState({page: this.state.page + 1});
+    this.setState({ page: this.state.page + 1 });
     this.updateNews();
   };
 
   render() {
     return (
       <div className="container my-3">
-        <h1 className="text-center" style={{fontWeight: '600', fontFamily: 'sans-serif'}}>Khabarwala - {this.props.category}</h1>
-        {this.state.loading && <Spinner />}
+        {this.state.loading ? <Spinner />:<h1
+            className="text-center"
+            style={{ fontWeight: "600", fontFamily: "sans-serif" }}
+          >
+            Khabarwala - {this.props.category}
+          </h1>}
         <div className="row">
           {!this.state.loading &&
             this.state.articles.map((element) => {
@@ -84,8 +84,8 @@ export default class News extends Component {
                     imageUrl={element.image}
                     newsUrl={element.url}
                     source={element.source.name}
-                    author = {element.author}
-                    date = {element.publishedAt}
+                    author={element.author}
+                    date={element.publishedAt}
                   />
                 </div>
               );
